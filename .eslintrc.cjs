e7f5b3de7bb79a96e73ee9e2618411ab22113874
Alan Cleyton <alancleyton67@gmail.com>
2025-01-27 19:16:21 -0300

    chore: add eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,38 @@
+/*eslint no-undef: off*/
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:vue/vue3-recommended"],
-  parser: "vue-eslint-parser",
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["vue"],
-  ignorePatterns: ["node_modules", "dist"],
+  plugins: ['vue'],
+  ignorePatterns: ['node_modules', 'dist'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // enforce 2 spaces indentation
+    indent: ['error', 2],
+
+    // enforces consistent use of trailing commas in object and arrays
+    'comma-dangle': ['error', 'always-multiline'],
+
+    // enforces semicolons at the end of each statement
+    semi: ['error', 'always'],
+
+    // enforce consistent spacing inside braces
+    'object-curly-spacing': ['error', 'always'],
+
+    // enforce use of single quotes
+    quotes: ['error', 'single', { avoidEscape: true }],
+
+    // require parentheses around arrow function only needed
+    'arrow-parens': ['error', 'as-needed'],
   },
 };
